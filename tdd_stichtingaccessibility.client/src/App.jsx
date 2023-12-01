88a243b4c71764f72fc48aa0e610a9e40846d140
williamvdm <williamvdm@live.nl>
2023-12-02 00:06:53 +0100

    fixed stuff

--- tdd_stichtingaccessibility.client/src/App.jsx
+++ tdd_stichtingaccessibility.client/src/App.jsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react';
-import { GoogleLogin } from '@react-oauth/google'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+/*Import hieronder de pagina die je hebt aangemaakt*/
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import './App.css';
@@ -10,6 +9,7 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
+                {/*Voeg onderaan een nieuwe route toe met de import*/}
                 <Route index element={<Home />} />
                 <Route path="/" element={<Home />} />
                 <Route path="/dashboard" element={<Dashboard />} />
