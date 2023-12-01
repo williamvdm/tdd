7eed05675ada3131a641721df96aea0c5ae47edc
williamvdm <williamvdm@live.nl>
2023-12-01 15:51:43 +0100

    Add project files.

---
+++ tdd_stichtingaccessibility.client/src/App.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { GoogleLogin } from '@react-oauth/google';
+import './App.css';
+
+function App() {
+    const responseMessage = (response) => {
+        console.log("Inlog succes")
+        console.log(response);
+    };
+    const errorMessage = (error) => {
+        console.log("Inlog gefaald")
+        console.log(error);
+    };
+
+    return (
+        <div>
+            <h2>Aanmelden</h2>
+            <br />
+            <GoogleLogin
+                theme="filled_blue"
+                size="large"
+                text="signup_with"
+                width="300px"
+                onSuccess={responseMessage}
+                onError={errorMessage} />
+        </div>
+    );
+}
+
+export default App;
---
+++ tdd_stichtingaccessibility.client/src/assets/auth/config/google_auth.jsx
@@ -0,0 +1,4 @@
+export const google_auth = {
+    clientId: '564563511455-a3hop6b9n311gcj8gi6o930gua1hmv4r.apps.googleusercontent.com',
+    redirectUri: 'http://localhost:5173', 
+};
