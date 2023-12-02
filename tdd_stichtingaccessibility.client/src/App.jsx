58937d966f37653a3e2d73cd1f467a446d411024
williamvdm <williamvdm@live.nl>
2023-12-02 21:33:46 +0100

    added: error

--- tdd_stichtingaccessibility.client/src/App.jsx
+++ tdd_stichtingaccessibility.client/src/App.jsx
@@ -5,7 +5,7 @@ import Dashboard from './pages/Dashboard';
 import './App.css';
 
 function App() {
-
+    console.error("Error: no errors")
     return (
         <BrowserRouter>
             <Routes>
