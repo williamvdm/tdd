ca0b6d4e83a62cee2528b5d8d009001f52bb68d8
williamvdm <williamvdm@live.nl>
2023-12-02 21:29:37 +0100

    stuff

--- tdd_stichtingaccessibility.client/src/components/Login.jsx
+++ tdd_stichtingaccessibility.client/src/components/Login.jsx
@@ -1,29 +1,14 @@
-import { useEffect } from 'react';  // Import useEffect from React
-
 import { GoogleLogin } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
-import { jwtDecode } from "jwt-decode";
-import { useUser } from '../store/UserStore';
-
-function extractUserInformation(response) {
-    return jwtDecode(response.credential);
-}
+import { jwtDecode } from 'jwt-decode';
 
 function Login() {
     const navigate = useNavigate();
-    const { user, setUser } = useUser();
-
-    useEffect(() => {
-        console.log("Current User:", user); /*Deze geeft de user*/
-
-        if (user) {
-            navigate('/dashboard');
-        }
-    }, [user]);
 
     const responseMessage = (response) => {
         console.log("Inlog succes");
-        setUser(jwtDecode(response.credential));
+        console.log(jwtDecode(response.credential));
+        navigate('/dashboard', { credential: response.credential });
     };
 
     const errorMessage = (error) => {
--- tdd_stichtingaccessibility.client/src/pages/Dashboard.jsx
+++ tdd_stichtingaccessibility.client/src/pages/Dashboard.jsx
@@ -1,13 +1,5 @@
-import { useEffect } from 'react';
-import { useUser } from '../store/UserStore';
 
 function Dashboard() {
-    const { user } = useUser();
-
-    useEffect(() => {
-        console.log("Dashboard: ", user); /*Deze geeft undefined maar moet ook de user geven*/
-    }, [user]);
-
     return (
         <h1>Dashboard</h1>
     );
