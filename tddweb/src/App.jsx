f9e463d83835b91b4d006b64a480ae9544c96530
Will <williamvdm@live.nl>
2023-12-11 15:12:51 +0100

    ädd: google oauth login/register

--- tddweb/src/App.jsx
+++ tddweb/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { GoogleLogin } from '@react-oauth/google';
 import './App.css'
 
 function App() {
   return (
     <>
-      
+      <GoogleLogin
+        onSuccess={credentialResponse => {
+          console.log(credentialResponse);
+        }}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+      />
     </>
   )
 }
