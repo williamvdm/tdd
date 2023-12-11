dea4710ba613a84f3bce417d72d66c757f8729eb
Will <williamvdm@live.nl>
2023-12-11 19:52:03 +0100

    updated front-end

--- tddweb/src/App.jsx
+++ tddweb/src/App.jsx
@@ -13,6 +13,10 @@ function App() {
           console.log('Login Failed');
         }}
       />
+
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Button
+      </button>
     </>
   )
 }
