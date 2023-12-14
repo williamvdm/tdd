57348c4a4f9a2dfda5c65de111de4fa7b4eebb03
Akram Achahboun <19106572@student.hhs.nl>
2023-12-14 13:12:09 +0100

    add: ervaringsdeskundigeportaal

--- tddweb/src/components/Nav.jsx
+++ tddweb/src/components/Nav.jsx
@@ -7,6 +7,8 @@ export default function Nav() {
                 <a href="/" class="text-black text-lg font-bold text-2xl">Ablox</a>
                 <ul class="flex space-x-4">
                     <li><a href="/login" class="text-black hover:underline-offset-8 hover:underline">Inloggen</a></li>
+                    <li><a href="/ervaringsdeskundigeportaal" class="text-black hover:underline-offset-8 hover:underline">Ervaringsdeskundigeportaal</a></li>
+
                 </ul>
             </div>
         </nav>
