61e0e3a1d385063df656299d6892b4aad6ffcf7f
Will <williamvdm@live.nl>
2023-12-14 09:50:29 +0100

    fixed css

--- tddweb/src/main.jsx
+++ tddweb/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.jsx'
-import './index.css'
+import './index.pcss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId="564563511455-a3hop6b9n311gcj8gi6o930gua1hmv4r.apps.googleusercontent.com">
