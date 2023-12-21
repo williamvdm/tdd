d57a0d40b34e520bdd6a9ab1ed3cb293cbefec82
Laurens <laatmijnueffe@gmail.com>
2023-12-21 13:31:12 +0100

    fix: layout

--- tddweb/src/pages/onderzoek.jsx
+++ tddweb/src/pages/onderzoek.jsx
@@ -2,32 +2,52 @@ const Onderzoek = () => {
     return (
         <>
             <h1>Onderzoek</h1>
-            
+
             <div style={{ display: 'flex', marginLeft: '5vw' }}>
 
-                <div style={{ backgroundColor: 'black', width: '20vh', height: '60vw' }}></div>
+                <div style={{ width: '30vh', height: '60vw', backgroundColor: 'pink' }}>
+                    Lorem ipsum
+                </div>
 
-                <div style={{ backgroundColor: 'grey', width: '50vw',  display: 'flex', flexDirection: 'column', marginBottom: '10vw' }}>
-                    
-                    <div style={{ backgroundColor: 'pink', width: '30vh', height: '20vw', marginBottom: '5vw', marginLeft: '10vw', marginTop: '5vw' }}></div>
-                    <div style={{ backgroundColor: 'blue', width: '30vh', height: '20vw', marginBottom: '5vw', marginLeft: '10vw', marginTop: '5vw' }}></div>
+                <div style={{ width: '30vw', display: 'flex', flexDirection: 'column', marginBottom: '10vw' }}>
+
+                    <div style={{ width: '30vh', height: '20vw', marginBottom: '5vw', marginLeft: '10vw', marginTop: '5vw' }}>
+                        <img src="/src/assets/stuff.png" width='70%'></img>
+                        Lorem ipsum
+                    </div>
+                    <div style={{ width: '30vh', height: '20vw', marginBottom: '5vw', marginLeft: '10vw', marginTop: '5vw' }}>
+                        <img src="/src/assets/stuff.png" width='70%'></img>
+                        Lorem ipsum
+                    </div>
 
                 </div>
 
-                <div style={{ backgroundColor: 'grey', width: '50vw', display: 'flex', flexDirection: 'column', marginBottom: '10vw' }}>
-                    
-                    <div style={{ backgroundColor: 'pink', width: '30vh', height: '20vw', marginBottom: '5vw', marginLeft: '10vw', marginTop: '5vw' }}></div>
-                    <div style={{ backgroundColor: 'blue', width: '30vh', height: '20vw', marginBottom: '5vw', marginLeft: '10vw', marginTop: '5vw' }}></div>
+                <div style={{ width: '30vw', display: 'flex', flexDirection: 'column', marginBottom: '10vw' }}>
+
+                    <div style={{ width: '30vh', height: '20vw', marginBottom: '5vw', marginLeft: '10vw', marginTop: '5vw' }}>
+                        <img src="/src/assets/stuff.png" width='70%'></img>
+                        Lorem ipsum
+                    </div>
+                    <div style={{ width: '30vh', height: '20vw', marginBottom: '5vw', marginLeft: '10vw', marginTop: '5vw' }}>
+                        <img src="/src/assets/stuff.png" width='70%'></img>
+                        Lorem ipsum
+                    </div>
 
                 </div>
 
-                <div style={{ backgroundColor: 'grey', width: '50vw', display: 'flex', flexDirection: 'column', marginBottom: '10vw' }}>
-                    
-                    <div style={{ backgroundColor: 'pink', width: '30vh', height: '20vw', marginBottom: '5vw', marginLeft: '10vw', marginTop: '5vw' }}></div>
-                    <div style={{ backgroundColor: 'blue', width: '30vh', height: '20vw', marginBottom: '5vw', marginLeft: '10vw', marginTop: '5vw' }}></div>
+                <div style={{ width: '30vw', display: 'flex', flexDirection: 'column', marginBottom: '10vw', marginRight: '5vw' }}>
+
+                    <div style={{ width: '30vh', height: '20vw', marginBottom: '5vw', marginLeft: '10vw', marginTop: '5vw' }}>
+                        <img src="/src/assets/stuff.png" width='70%'></img>
+                        Lorem ipsum
+                    </div>
+                    <div style={{ width: '30vh', height: '20vw', marginBottom: '5vw', marginLeft: '10vw', marginTop: '5vw' }}>
+                        <img src="/src/assets/stuff.png" width='70%'></img>
+                        Lorem ipsum
+                    </div>
 
                 </div>
-            
+
             </div>
         </>
     );
