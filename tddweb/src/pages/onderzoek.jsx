5c26ff33ba7d3846fde7a9bdaa7e35ce8ea6ecbb
williamvdm <williamvdm@live.nl>
2024-01-07 19:52:09 +0100

    added dropdown for filter menu

--- tddweb/src/pages/onderzoek.jsx
+++ tddweb/src/pages/onderzoek.jsx
@@ -103,6 +103,22 @@ const Onderzoek = () => {
     ]);
     const [searchInput, setSearchInput] = useState('');
     const [searchedOnderzoeken, setSearchedOnderzoeken] = useState(onderzoeken);
+    const [filteredTags, setFilteredTags] = useState([]);
+    const [menuOpen, setMenuOpen] = useState(true);
+
+    useEffect(() => {
+        const uniqueTags = new Set();
+
+        onderzoeken.forEach((onderzoek) => {
+            onderzoek.tags.forEach((tag) => {
+                uniqueTags.add(tag);
+            });
+        });
+
+        const uniqueTagsArray = Array.from(uniqueTags);
+
+        setFilteredTags(uniqueTagsArray);
+    }, [onderzoeken]);
 
     useEffect(() => {
         if (searchInput) {
@@ -117,17 +133,32 @@ const Onderzoek = () => {
         }
     }, [searchInput, onderzoeken]);
 
+    function filterDropdown() {
+        let menu = document.getElementById("filter_menu");
+        setMenuOpen(!menuOpen);
+        menu.style.visibility = menuOpen ? "visible" : "hidden";
+    }
+
     return (
         <div className="container">
             <h1 className="text-3xl py-10 font-bold">Lopende onderzoeken</h1>
-            <div className="flex flex-row justify-end ms:flex-col">
+            <div className="flex flex-row justify-end ms:flex-col ms:w-full">
                 <form className="flex flex-row p-4 mb-4 rounded-lg bg-white p-6 border border-accessgray w-1/3 mr-4 relative">
-                    <input id="search_input" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} className="transition ease-in-out w-full focus:outline-none" placeholder="Klik hier om te zoeken" />
+                    <input id="search_input" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} className="transition ease-in-out w-full h-full focus:outline-none" placeholder="Klik hier om te zoeken" />
                 </form>
-                <button className="p-4 mb-4 rounded-lg bg-white p-6 border border-accessgray ml-1">Filter</button>
+                <button className="p-4 mb-4 rounded-lg bg-white p-6 border border-accessgray ml-1" onClick={filterDropdown}>Filter</button>
+                <div id="filter_menu" className="rounded-lg bg-white border border-accessgray absolute p-4 mt-20 invisible">
+                    {filteredTags.map((filteredTag) => (
+                        <div key={filteredTag} className="p-1">
+                            <input type="checkbox" id={filteredTag} name={filteredTag} value={filteredTag} />
+                            <label className="ml-2" htmlFor={filteredTag}>{filteredTag}</label>
+                        </div>
+                    ))}
+                </div>
             </div>
 
-            {onderzoeken &&
+            {
+                onderzoeken &&
                 <div className="flex justify-around flex-col items-center">
                     {searchedOnderzoeken.length == 0 && <div>Geen zoekresultaten...</div>}
                     {searchedOnderzoeken.map(onderzoek => (
@@ -151,7 +182,7 @@ const Onderzoek = () => {
                     ))}
                 </div>
             }
-        </div>
+        </div >
     );
 };
 
