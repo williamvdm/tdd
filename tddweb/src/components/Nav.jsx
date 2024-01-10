0c9304e0510a5cfc9232752e884abf2c4643e395
williamvdm <williamvdm@live.nl>
2024-01-10 17:18:20 +0100

    sorry develop ging per ongeluk maar dashboard voor ervaringsdeskundigen

--- tddweb/src/components/Nav.jsx
+++ tddweb/src/components/Nav.jsx
@@ -4,17 +4,9 @@ export default function Nav() {
     return (
         <nav className="shadow-md mb-6 p-4 bg-accessblue">
             <div className="container mx-auto flex justify-between items-center">
-                <Link to="/" className="text-white text-lg font-bold text-2xl">Ablox</Link>
+                <Link to="/" className="text-white text-lg font-bold text-2xl" aria-label="Logo van Ablox">Ablox</Link>
                 <ul className="flex space-x-4">
                     <li><Link to="/login" className="text-white hover:underline-offset-8 hover:underline">Inloggen</Link></li>
-                    <li className="group">
-                        <span className="text-white hover:underline-offset-8 hover:underlin">Portaal</span>
-                        <ul className="hidden group-hover:block absolute z-10">
-                            <li><Link to="/portaal/ervaringsdeskundige" className="text-black hover:underline">Ervaringsdeskundige</Link></li>
-                            <li><Link to="/portaal/bedrijven" className="text-black hover:underline">Bedrijven</Link></li>
-                            <li><Link to="/dashboard/onderzoek" className="text-black hover:underline">Onderzoek</Link></li>
-                        </ul>
-                    </li>
                     <li><Link to="/chat" className="text-white hover:underline-offset-8 hover:underline">Chat</Link></li>
                 </ul>
             </div>
