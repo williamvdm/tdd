3a25362492f04b29eeb7ad4a1449c0bdcdf5adf9
Ann <itsumowarattene@hotmail.com>
2024-01-11 14:25:33 +0100

    zichtbaarheid icoon voor wachtwoord toegevoegd

--- tddweb/src/pages/login.jsx
+++ tddweb/src/pages/login.jsx
@@ -1,10 +1,21 @@
 import { GoogleLogin } from '@react-oauth/google';
+import { useState } from 'react';
+import { EyeInvisibleOutlined, EyeOutlined } from "@ant-design/icons";
 
 function Login() {
+    const [showPassword, setShowPassword] = useState(false);
+
     const handleLogin = (event) => {
         event.preventDefault();
         console.log('Formulier verzonden');
     };
+
+    const handleTogglePassword = () => {
+        setShowPassword(!showPassword);
+
+}
+
+
 
     return (
         <div class="min-h-screen font-muli bg-accessbluebg pt-20">
@@ -34,7 +45,7 @@ function Login() {
                     <div class="relative h-10 w-full min-w-[200px]">
                         <input 
                         required
-                        type="password"
+                        type={showPassword ? "text" : "password"}
                         class="hover:border-b-4 border-orange-500 peer h-full w-full rounded-[7px] border border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                         placeholder=" " />
                         <label
@@ -43,6 +54,13 @@ function Login() {
                         class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l  before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                         Wachtwoord (verplicht)
                         </label>
+                        <button
+                            type='button'
+                            className='absolute right-0 top-1/2 transform -translate-y-1/2 mr-2 cursor-pointer'
+                            onClick={handleTogglePassword}
+                        >
+                            {showPassword ? <EyeInvisibleOutlined/> : <EyeOutlined/>}
+                        </button>
                     </div>
                     </div>
 
