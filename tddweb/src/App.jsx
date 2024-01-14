990a4172df073d964aa42d4c2ee47a2c64e3f834
williamvdm <williamvdm@live.nl>
2024-01-14 13:11:28 +0100

    fixed not logged in navigation possible to dashboard

--- tddweb/src/App.jsx
+++ tddweb/src/App.jsx
@@ -17,11 +17,8 @@ function App() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // Check if a token is present in localStorage
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
 
-    // If there is no token and the user is trying to access a protected route,
-    // navigate them to the login page, except for the root route "/"
     if (!token && !window.location.pathname.includes('/login') && window.location.pathname !== '/') {
       navigate('/login');
     }
@@ -39,8 +36,6 @@ function App() {
           <Route path="/dashboard/onderzoek" element={<Onderzoek />} />
           <Route path="/onderzoek/:onderzoekid" element={<OnderzoekDetail />} />
           <Route path="/chat" element={<Chat />} />
-          {/* You can add more routes as needed */}
-          {/* Non-existent routes will be handled by the default redirect */}
           <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
       </div>
