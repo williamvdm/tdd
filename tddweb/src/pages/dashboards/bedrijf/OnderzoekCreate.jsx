12387d3452db0b5675ac220eef8be3d11ee00c08
williamvdm <williamvdm@live.nl>
2024-01-21 14:47:44 +0100

    styling voor onderzoek creeeren pagina

--- tddweb/src/pages/dashboards/bedrijf/OnderzoekCreate.jsx
+++ tddweb/src/pages/dashboards/bedrijf/OnderzoekCreate.jsx
@@ -67,56 +67,57 @@ export default function OnderzoekCreate() {
 
   return (
     <div className="container mx-auto my-10 p-6 bg-white border border-gray-300 rounded-lg shadow-lg max-w-screen-md">
+      <h1 className="text-2xl font-bold mb-4">Onderzoek creeeren</h1>
       <form onSubmit={handleCreate}>
-        <label>
+        <label className="block mb-2">
           Titel:
           <input
             type="text"
             name="titel"
-            className="border-gray-300 border p-2 rounded"
+            className="border p-2 rounded w-full focus:outline-accessblue focus:border-accessblue"
             value={onderzoekData.titel}
             onChange={handleInputChange}
           />
         </label>
         <br />
-        <label>
+        <label className="block mb-2">
           Beschrijving:
           <textarea
             name="beschrijving"
-            className="border-gray-300 border p-2 rounded"
+            className="border p-2 rounded w-full focus:outline-accessblue focus:border-accessblue"
             value={onderzoekData.beschrijving}
             onChange={handleInputChange}
           />
         </label>
         <br />
-        <label>
+        <label className="block mb-2">
           Begindatum:
           <input
             type="date"
             name="begindatum"
-            className="border-gray-300 border p-2 rounded"
+            className="border p-2 rounded w-full focus:outline-accessblue focus:border-accessblue"
             value={onderzoekData.begindatum}
             onChange={handleInputChange}
           />
         </label>
         <br />
-        <label>
+        <label className="block mb-2">
           Einddatum:
           <input
             type="date"
             name="einddatum"
-            className="border-gray-300 border p-2 rounded"
+            className="border p-2 rounded w-full focus:outline-accessblue focus:border-accessblue"
             value={onderzoekData.einddatum}
             onChange={handleInputChange}
           />
         </label>
         <br />
-        <label>
+        <label className="block mb-2">
           Postcode:
           <input
             type="text"
             name="postCode"
-            className="border-gray-300 border p-2 rounded"
+            className="border p-2 rounded w-full focus:outline-accessblue focus:border-accessblue"
             value={onderzoekData.locatie.postCode}
             onChange={(e) =>
               setOnderzoekData((prevData) => ({
@@ -127,12 +128,12 @@ export default function OnderzoekCreate() {
           />
         </label>
         <br />
-        <label>
+        <label className="block mb-2">
           Plaatsnaam:
           <input
             type="text"
             name="plaatsNaam"
-            className="border-gray-300 border p-2 rounded"
+            className="border p-2 rounded w-full focus:outline-accessblue focus:border-accessblue"
             value={onderzoekData.locatie.plaatsNaam}
             onChange={(e) =>
               setOnderzoekData((prevData) => ({
@@ -143,12 +144,12 @@ export default function OnderzoekCreate() {
           />
         </label>
         <br />
-        <label>
+        <label className="block mb-2">
           Straatnaam:
           <input
             type="text"
             name="straatNaam"
-            className="border-gray-300 border p-2 rounded"
+            className="border p-2 rounded w-full focus:outline-accessblue focus:border-accessblue"
             value={onderzoekData.locatie.straatNaam}
             onChange={(e) =>
               setOnderzoekData((prevData) => ({
@@ -159,12 +160,12 @@ export default function OnderzoekCreate() {
           />
         </label>
         <br />
-        <label>
+        <label className="block mb-2">
           Huisnummer:
           <input
             type="number"
             name="huisnummer"
-            className="border-gray-300 border p-2 rounded"
+            className="border p-2 rounded w-full focus:outline-accessblue focus:border-accessblue"
             value={onderzoekData.locatie.huisnummer}
             onChange={(e) =>
               setOnderzoekData((prevData) => ({
@@ -175,12 +176,12 @@ export default function OnderzoekCreate() {
           />
         </label>
         <br />
-        <label>
+        <label className="block mb-2">
           Beloning Beschrijving:
           <input
             type="text"
             name="beloningBeschrijving"
-            className="border-gray-300 border p-2 rounded"
+            className="border p-2 rounded w-full focus:outline-accessblue focus:border-accessblue"
             value={onderzoekData.beloningBeschrijving}
             onChange={handleInputChange}
           />
@@ -188,7 +189,7 @@ export default function OnderzoekCreate() {
         <br />
         <button
           type="submit"
-          className="mr-2 outline-none hover:outline-solid hover:outline-2 hover:outline-accessblue text-black p-2 px-4 rounded-lg transition ease-in-out flex items-center focus:outline-accessblue"
+          className="w-full bg-accessblue hover: text-white py-2 px-4 rounded outline-none hover:outline-solid hover:outline-2 hover:outline-accessblue"
         >
           Verder
         </button>
